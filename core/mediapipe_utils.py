c28cd09f9b8acbb08e0ff08a3f964dfee02c0a22
Manuel Alvarez Iglesias <manoalvarez2023@gmail.com>
2025-12-08 04:21:09 +0000

    Merge pull request #2 from Manuel-Alvarez05/main
    Changes

--- core/mediapipe_utils.py
+++ core/mediapipe_utils.py
@@ -101,8 +101,18 @@ def init_webrtc_stream(key: str):
         key=key,
         mode=WebRtcMode.SENDRECV,
         video_processor_factory=MediaPipeHandProcessor,
-        media_stream_constraints={"video": True, "audio": False},
+        media_stream_constraints={
+            "video": {"width": {"min": 640, "ideal": 1280, "max": 1920}},
+            "audio": False
+        },
         async_processing=True,
+        rtc_configuration={"iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {"urls": ["stun:stun3.l.google.com:19302"]},
+            {"urls": ["stun:stun4.l.google.com:19302"]}
+        ]},
     )
 
 
